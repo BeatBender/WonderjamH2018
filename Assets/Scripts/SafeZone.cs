be7db99be35abd28dd3a7bc9dbecc8827edc9c4f
Patrick Levesque <levesque_patrick_2@hotmail.com>
2018-03-03 17:33:22 -0500

    Changement de png en sprites encore

---
+++ Assets/Scripts/SafeZone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour {
+
+    bool isPlayer1Safe, isPlayer2Safe;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player1")
+        {
+            isPlayer1Safe = true;
+        }
+
+        if (collision.name == "Player2")
+        {
+            Debug.Log("Safe");
+            isPlayer2Safe = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player1")
+        {
+            isPlayer1Safe = false;
+        }
+
+        if (collision.name == "Player2")
+        {
+            Debug.Log("Not Safe");
+            isPlayer2Safe = false;
+        }
+    }
+}
