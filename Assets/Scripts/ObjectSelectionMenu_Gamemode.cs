258d388837034273cab20bc11af8b58a45314e1a
alexandrain <alexandrain90808@hotmail.fr>
2018-03-03 10:36:59 -0500

    update

--- Assets/Scripts/ObjectSelectionMenu_Gamemode.cs
+++ Assets/Scripts/ObjectSelectionMenu_Gamemode.cs
@@ -6,28 +6,30 @@ using UnityEngine.UI;
 
 public class ObjectSelectionMenu_Gamemode : MonoBehaviour {
 
-    public RectTransform navigator1;
-    int nav1Pos = 0;
-    public RectTransform navigator2;
-    int nav2Pos = 0;
- 
+	public RectTransform navigator1;
+	int nav1Pos = 0;
+	public RectTransform navigator2;
+	int nav2Pos = 0;
+
 	public RectTransform[] slots = new RectTransform[3];
-    public RectTransform[] slotJ1 = new RectTransform[3];
+	public RectTransform[] slotJ1 = new RectTransform[3];
 	private int objectJ1;
-    private int objectJ2;
+	private int objectJ2;
 	public RectTransform[] slotJ2 = new RectTransform[3];
 	public int jumpAmount;
 	public float timeToWaitJ1, timeToWaitJ2;
 	private float waitingTime;
 
-	private RawImage[] objectPlaceJ1 = new RawImage[3] ;
-	private RawImage[] objectPlaceJ2 = new RawImage[3];
+	public GameObject parentInCanvasJ1, parentInCanvasJ2;
 
-	public RawImage[] imageObject= new RawImage[3];
-    
+	private GameObject[] objectPlaceJ1 = new GameObject[3] ;
+	private GameObject[] objectPlaceJ2 = new GameObject[3];
+
+	public GameObject[] imageObject= new GameObject[3];
+
 	void Start(){
-    	MoveNav1(0);
-  	    MoveNav2(0);
+		MoveNav1(0);
+		MoveNav2(0);
 		timeToWaitJ1 = (float)0.3;
 		timeToWaitJ2 = (float)0.3;
 		waitingTime = timeToWaitJ1;
@@ -35,17 +37,17 @@ public class ObjectSelectionMenu_Gamemode : MonoBehaviour {
 		timeToWaitJ2 = 0;
 	}
 
-     void Update () {
-         // move up
+	void Update () {
+		// move up
 		if(Input.GetAxis("VerticalPlayer1") != 0 && timeToWaitJ1 <= 0 ){
 			timeToWaitJ1 = waitingTime;
 			MoveNav1((float)-jumpAmount * Input.GetAxis("VerticalPlayer1"));
-		 }
+		}
 		if(Input.GetAxis("VerticalPlayer2")!= 0 && timeToWaitJ2 <= 0 ){
 			timeToWaitJ2 = waitingTime;
 			MoveNav2((float)-jumpAmount *  Input.GetAxis("VerticalPlayer2"));
 		}
- 
+
 		if(Input.GetAxis("HorizontalPlayer1")!= 0 && timeToWaitJ1 <=0.0f ){
 			timeToWaitJ1 = waitingTime;
 			MoveNav1(Input.GetAxis("HorizontalPlayer1"));
@@ -59,71 +61,102 @@ public class ObjectSelectionMenu_Gamemode : MonoBehaviour {
 			switch (nav1Pos) {
 			case 0:
 				{
-					Vector3 spawnPosition = new Vector3 (slotJ1 [0].position.x, slotJ1 [0].position.y, slotJ1 [0].position.z);
+					Vector3 spawnPosition = new Vector3 (slotJ1 [0].position.x, slotJ1 [0].position.y, slotJ1 [0].position.z+1);
 					Quaternion spawnRotation = Quaternion.identity;
-					objectPlaceJ1[objectJ1] = Instantiate (imageObject [0], spawnPosition, spawnRotation) as RawImage;
+					objectPlaceJ1[objectJ1] = Instantiate (imageObject [0], spawnPosition, spawnRotation) as GameObject;
+					objectPlaceJ1 [objectJ1].transform.SetParent (parentInCanvasJ1.transform, false);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.localScale = new Vector3 (0.3f, 0.2f, 1);
+					objectPlaceJ1 [objectJ1].transform.position = new Vector3 (slotJ1 [objectJ1].position.x, slotJ1 [objectJ1].position.y, slotJ1 [objectJ1].position.z	);
 
-
+					objectJ1++;
 				}
 				break;
 			case 1:
 				{
+					Vector3 spawnPosition = new Vector3 (0, 0, 0);
+					Quaternion spawnRotation = Quaternion.identity;
+					objectPlaceJ1[objectJ1] = Instantiate (imageObject [1], spawnPosition, spawnRotation) as GameObject;
+					objectPlaceJ1 [objectJ1].transform.SetParent (parentInCanvasJ1.transform, false);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.localScale = new Vector3 (0.3f, 0.2f, 1);
+					objectPlaceJ1 [objectJ1].transform.position = new Vector3 (slotJ1 [objectJ1].position.x, slotJ1 [objectJ1].position.y, slotJ1 [objectJ1].position.z	);
+
+					objectJ1++;
 				}
 				break;
 			case 2:
 				{
-			}
+					Vector3 spawnPosition =  new Vector3 (0, 0, 0);
+					Quaternion spawnRotation = Quaternion.identity;
+					objectPlaceJ1[objectJ1] = Instantiate (imageObject [2], spawnPosition, spawnRotation) as GameObject;
+					objectPlaceJ1 [objectJ1].transform.SetParent (parentInCanvasJ1.transform, false);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0);
+					objectPlaceJ1 [objectJ1].transform.localScale = new Vector3 (0.3f, 0.2f, 1);
+					objectPlaceJ1 [objectJ1].transform.position = new Vector3 (slotJ1 [objectJ1].position.x, slotJ1 [objectJ1].position.y, slotJ1 [objectJ1].position.z	);
+
+					objectJ1++;
+				}
 				break;
 			}
 		}
 		if (Input.GetKeyDown (KeyCode.Joystick2Button0)) {
-//			switch (nav2Pos) {
-//			case 0:
-//				slotJ2[objectJ2] = new RectTransform (slots [0]);
-//				break;
-//			case 1:
-//				slotJ2[objectJ2] = new RectTransform (slots [1]);
-//			case 2:
-//				slotJ2[objectJ2] = new RectTransform (slots [2]);
-//				}
+			//			switch (nav2Pos) {
+			//			case 0:
+			//				slotJ2[objectJ2] = new RectTransform (slots [0]);
+			//				break;
+			//			case 1:
+			//				slotJ2[objectJ2] = new RectTransform (slots [1]);
+			//			case 2:
+			//				slotJ2[objectJ2] = new RectTransform (slots [2]);
+			//				}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Joystick1Button1) && objectJ1 >0) {
+			Destroy(objectPlaceJ1 [objectJ1-1]);
+			objectJ1--;
 		}
 		float temp = Time.deltaTime;
 		timeToWaitJ1 -= temp;
 		timeToWaitJ2 -= temp;
-     }
- 
-     void MoveNav1(float change){
-         if(change > 0){
-             if(nav1Pos+1 < slots.Length-1){
-                 nav1Pos += 1;
-             }else{
-                 nav1Pos = slots.Length-1;
-             }
-         }else{
-             if(nav1Pos-1 >= 0){
-                 nav1Pos += -1;
-             }else{
-                 nav1Pos = 0;
-             }
-         }
-         navigator1.position = slots[nav1Pos].position;
-     }
- 
-     void MoveNav2(float change){
-         if(change > 0){
-             if(nav2Pos+1 < slots.Length-1){
-                 nav2Pos += 1;
-             }else{
-                 nav2Pos = slots.Length-1;
-             }
-         }else{
-             if(nav2Pos-1 >= 0){
-                 nav2Pos += -1;
-             }else{
-                 nav2Pos = 0;
-             }
-         }
-         navigator2.position = slots[nav2Pos].position;
-     }
- }
+	}
+
+	void MoveNav1(float change){
+		if(change > 0){
+			if(nav1Pos+1 < slots.Length-1){
+				nav1Pos += 1;
+			}else{
+				nav1Pos = slots.Length-1;
+			}
+		}else{
+			if(nav1Pos-1 >= 0){
+				nav1Pos += -1;
+			}else{
+				nav1Pos = 0;
+			}
+		}
+		navigator1.position = slots[nav1Pos].position;
+	}
+
+	void MoveNav2(float change){
+		if(change > 0){
+			if(nav2Pos+1 < slots.Length-1){
+				nav2Pos += 1;
+			}else{
+				nav2Pos = slots.Length-1;
+			}
+		}else{
+			if(nav2Pos-1 >= 0){
+				nav2Pos += -1;
+			}else{
+				nav2Pos = 0;
+			}
+		}
+		navigator2.position = slots[nav2Pos].position;
+	}
+}
+
  
