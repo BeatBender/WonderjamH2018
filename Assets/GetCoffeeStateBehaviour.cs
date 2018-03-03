8b85241fce1a71be028240a0d0e8ea47583d28f8
DESKTOP-AO31ARM\PYGLI <pierreyves.glidden@hotmail.com>
2018-03-03 14:40:06 -0500

    abdenour va boire du café... enfin

---
+++ Assets/GetCoffeeStateBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCoffeeStateBehaviour : StateMachineBehaviour {
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        TeacherAi teacherAi = animator.gameObject.GetComponent<TeacherAi>();
+        teacherAi.getCoffee();
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        TeacherAi teacherAi = animator.gameObject.GetComponent<TeacherAi>();
+        teacherAi.stopDrinkingCoffee();
+    }
+    
+}
