57d4beb5488fed2867f5975819a79f21dc89deb5
matpoi <mathieu_poirel@hotmail.com>
2018-03-03 02:28:23 -0500

    score+aiguille+reactionProf

---
+++ Assets/Scripts/Mat/Anger_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anger_Script : MonoBehaviour {
+
+	RectTransform rectTransform;
+	float max_anger = 180f;
+	public static bool Inline;
+	public static float Angerlvl;
+	 float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		rectTransform = GetComponent<RectTransform>();
+		Angerlvl = 0f;
+		Inline = false;
+		speed = 0.5f;
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Inline == true) {
+
+			rectTransform.Rotate( new Vector3( 0, 0, -speed ) );
+
+			Angerlvl+= speed;
+			Debug.Log (Angerlvl);
+		}
+
+
+
+	}
+}
