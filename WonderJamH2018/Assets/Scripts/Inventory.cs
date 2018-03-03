110347a0bce13065b77a981acb6d16f85eccda44
alexandrain <alexandrain90808@hotmail.fr>
2018-03-02 19:06:01 -0500

    Control + Menu Inventory

--- WonderJamH2018/Assets/Scripts/Inventory.cs
+++ WonderJamH2018/Assets/Scripts/Inventory.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour {
 
+	private struct Object{
+		private int id { get; }
+		private int quantity { get;} 
+		}
+
+	private Object[] player_Inventory; 
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
 	// Use this for initialization
 	void Start () {
-		
+		this.player_Inventory = new Object[]{}; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
---
+++ WonderJamH2018/Assets/Scripts/ObjectSelectionMenu_Gamemode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSelectionMenu_Gamemode : MonoBehaviour {
+
+		// Use this for initialization
+	public RectTransform selector1; 
+	public int select1Pos = 0;
+	public RectTransform selector2; 
+	public int select2Pos= 0;
+
+	public int numberObject; 
+	public RectTransform[] slots = new RectTransform[numberObject];
+
+
+	public Text textShowSelect1;
+	public Text textShowSelect2;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
